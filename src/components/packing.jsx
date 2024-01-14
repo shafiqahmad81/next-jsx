60ec529b1b55198aaf11e51b55a925be52b6133f
shafiqahmad81 <shafiqahmad081@gmail.com>
2024-01-15 05:19:58 +0600

    update packing.jsx

---
+++ src/components/packing.jsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line react/prop-types
+function Item({ name, isPacked }) {
+  let itemContent = "";
+
+  if (isPacked) {
+    itemContent = <span>{name + "✔"}</span>;
+  } else {
+    itemContent = name;
+  }
+
+  return <li className="item">{itemContent}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Rides Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
